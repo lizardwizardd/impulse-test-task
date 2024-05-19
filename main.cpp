8a141630accca15f4dcf15b987f15070ec0781dd
lizardwizardd <milovankin65@gmail.com>
2024-05-19 15:40:05 +0300

    Fix incorrect header path

--- main.cpp
+++ main.cpp
@@ -1,7 +1,7 @@
 #include <iostream>
 
-#include "../inc/computerclub.hpp"
-#include "../inc/myexceptions.hpp"
+#include "inc/computerclub.hpp"
+#include "inc/myexceptions.hpp"
 
 int main(int argc, char** argv)
 {
